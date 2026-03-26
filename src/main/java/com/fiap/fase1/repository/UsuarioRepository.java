c86d7aa120427fc1ad9bba9de5d5ab66b0cdb50e
Jefferson Santos <jeffesa@gmail.com>
2026-03-26 00:44:18 -0300

    feat: adicionar métodos customizados no UsuarioRepository e testes

--- src/main/java/com/fiap/fase1/repository/UsuarioRepository.java
+++ src/main/java/com/fiap/fase1/repository/UsuarioRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByLogin(String login);
     boolean existsByEmail(String email);
+    boolean existsByLogin(String login);
 }
